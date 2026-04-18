89013e2ae784f35f96b2c4658311c1e959bce4c4
pqlpp <pqlpp@proton.me>
2026-04-18 12:50:15 +0900

    Update: アセンブリ情報のバージョンを更新し、キャッシュファイルを再生成

--- NoiceServer/obj/Debug/net8.0/NoiceServer.AssemblyInfo.cs
+++ NoiceServer/obj/Debug/net8.0/NoiceServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NoiceServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de306e295207effe900a70226f358a0261846d6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf42bb039be83bc9ce81c73e30372f71ebd62182")]
 [assembly: System.Reflection.AssemblyProductAttribute("NoiceServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NoiceServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
